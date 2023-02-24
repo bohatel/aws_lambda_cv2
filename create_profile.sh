6a9ea609af8dc7f362e5e371a4ff46e837a6f051
bohatel <razvan.bohatel@gmail.com>
2023-02-24 21:48:40 +0200

    role provisioning script

---
+++ create_profile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ROLE_NAME=lambda-opencv_exec
+
+aws iam create-role --role-name $ROLE_NAME --assume-role-policy-document '{"Version":"2012-10-17","Statement":{"Effect":"Allow","Principal":{"Service":"lambda.amazonaws.com"},"Action":"sts:AssumeRole"}}'
+[ $? -eq 0 ] && aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --role-name $ROLE_NAME
+[ $? -eq 0 ] && aws iam attach-role-policy --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole --role-name $ROLE_NAME
+
