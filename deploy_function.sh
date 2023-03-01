63a145ad98cbba937edc35cabd3213f7d257ada2
bohatel <razvan.bohatel@gmail.com>
2023-03-01 15:02:41 +0200

    add delay to allow the function creation to complete before attaching the libs layer

--- deploy_function.sh
+++ deploy_function.sh
@@ -13,6 +13,6 @@ rm -f app.zip
 zip app.zip app.py
 [ $? -eq 0 ] && aws s3 cp app.zip s3://$LAMBDA_BUCKET
 [ $? -eq 0 ] && aws lambda create-function --function-name $FUNCTION_NAME --timeout 20 --role arn:aws:iam::${ACCOUNT_ID}:role/$ROLE_NAME --handler app.lambda_handler --runtime python3.9 --code S3Bucket="$LAMBDA_BUCKET",S3Key="app.zip"
-
+[ $? -eq 0 ] && sleep 5
 [ $? -eq 0 ] && LAYER=$(aws lambda list-layer-versions --layer-name $LAYER_NAME | jq -r '.LayerVersions[0].LayerVersionArn')
 [ $? -eq 0 ] && aws lambda update-function-configuration --function-name $FUNCTION_NAME --layers $LAYER
